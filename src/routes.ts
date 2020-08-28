acb69cb9cb976c4bc43153c3fc232d315e9f6860
Thales Macêdo <thalesmacedo1@live.com>
2020-08-28 14:28:09 -0300

    added db.json as main storage file and added file system to routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
 const routes = express.Router();
@@ -15,17 +16,15 @@ interface UserRegister {
   queue: Array<Users>;
 }
 
-const data: UserRegister = {
-  users: [],
-  queue: [],
-};
-
 function avoidDuplicated(
   request: Request,
   response: Response,
   next: NextFunction,
 ) {
   const { id } = request.body;
+
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
 
   const isInQueue = Boolean(data.queue.find((user) => user.id === id));
   if (isInQueue) {
@@ -43,6 +42,9 @@ routes.post('/createUser', (request, response) => {
       .json({ error: 'Ops... O usuário deve ter um nome, email e gênero' });
   }
 
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
+
   const newUser = {
     id: uuidv4(),
     nome,
@@ -52,11 +54,15 @@ routes.post('/createUser', (request, response) => {
 
   data.users.push(newUser);
 
-  return response.json(newUser);
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json(newUser));
 });
 
 routes.post('/addToLine', avoidDuplicated, (request, response) => {
   const { id } = request.body;
+
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
 
   const registeredUser = data.users.findIndex((user: any) => user.id === id);
   if (registeredUser === -1) {
@@ -66,21 +72,29 @@ routes.post('/addToLine', avoidDuplicated, (request, response) => {
   data.queue.push(newQueueUser);
   const userIndex = data.queue.findIndex((user: any) => user.id === id);
 
-  return response.json({ posicao: userIndex + 1 });
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json({ posicao: userIndex + 1 }));
 });
 
 routes.get('/findPosition', (request, response) => {
   const { email } = request.query;
+
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
 
   const userIndex = data.queue.findIndex((user: any) => user.email === email);
   if (userIndex === -1) {
     return response.status(400).json({ error: 'Usuário não encontrado' });
   }
 
-  return response.json({ posicao: userIndex + 1 });
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json({ posicao: userIndex + 1 }));
 });
 
 routes.get('/showLine', (request, response) => {
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
+
   const formatedQueue = data.queue.map(({ nome, genero, email }, index) => ({
     nome,
     genero,
@@ -88,11 +102,15 @@ routes.get('/showLine', (request, response) => {
     posicao: index + 1,
   }));
 
-  response.json(formatedQueue);
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json(formatedQueue));
 });
 
 routes.get('/filterLine', (request, response) => {
   const { genero: gender } = request.query;
+
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
 
   const queueByGender = data.queue
     .map(({ nome, genero, email }, index) => ({
@@ -103,16 +121,20 @@ routes.get('/filterLine', (request, response) => {
     }))
     .filter((user) => user.genero === gender);
 
-  return response.json(queueByGender);
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json(queueByGender));
 });
 
 routes.delete('/popLine', (request, response) => {
+  const db = fs.readFileSync('db.json');
+  const data: UserRegister = JSON.parse(db.toString());
   if (data.queue.length === 0) {
     return response.status(400).json({ error: 'Ops... A fila está vazia' });
   }
 
   const first = data.queue.shift();
-  return response.json(first);
+  const temp = JSON.stringify(data, null, 2);
+  fs.writeFile('db.json', temp, () => response.json(first));
 });
 
 export default routes;
